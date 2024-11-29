0930d365ef585a5bcf4592a9edd9c3581ca3168a
XnopytAHH <s10269017@connect.np.edu.sg>
2024-11-30 00:27:00 +0800

    Added a form and the content to the join us! page

---
+++ joinform.js
@@ -0,0 +1,30 @@
+var form = document.getElementById('idForm');
+var idInput = document.getElementById('studid');
+document.write(acad)   ;
+form.addEventListener('submit', function (event) {
+  var idValue = idInput.value.trim();
+  
+  // Check if the ID is numeric and within the required length
+  if (!(idValue.length == 9)) {
+    event.preventDefault(); // Stop form submission
+    alert("Please input a valid student ID");
+    }
+  else {
+    var acad=document.getElementById('acad').value;
+    if (acad=="none") {
+      event.preventDefault(); // Stop form submission
+      alert("Please select an academic school");
+    }
+    else {
+      var year=document.getElementById('year').value;
+      if (year=="none") {
+        event.preventDefault(); // Stop form submission
+        alert("Please select a year");
+      }
+      else {
+        form.remove()
+        }
+    }
+    
+  }
+});
