5b568f266be841447639e7f4ad06755fb371b828
XnopytAHH <s10269017@connect.np.edu.sg>
2024-11-30 17:02:24 +0800

    Formatted pictures in performance tab, added navigation bar

---
+++ navigation.js
@@ -0,0 +1,10 @@
+function homepage() {
+    location.replace("index.html")
+}
+function performancepage() {
+    location.replace("performance.html")
+}
+
+function joinpage() {
+    location.replace("join.html")
+}
